6a7bda98b1a71e89db20fdc9a753be92e79835d6
Kirill Zabelin <zabelinkirillv@gmail.com>
2020-09-25 11:33:29 +0300

    Second lab is added

--- tasks/prolog/2_truth_table.pl
+++ tasks/prolog/2_truth_table.pl
@@ -10,3 +10,43 @@
 % true fail true
 % fail true fail
 % fail fail fail
+
+bool(true).
+bool(false).
+
+not(A) :-
+	\+ A.
+
+and(A, B) :-
+	A,
+	B.
+
+or(A, B) :-
+	not(and(not(A),	not(B))).
+
+xor(A, B) :-
+	or(and(A, not(B)), and(not(A), B)).
+
+equ(A, B) :-
+	not(xor(A, B)).
+
+evaluation(Expression, true) :- Expression, !.
+evaluation(Expression, false) :- not(Expression), !.
+
+truth_table(A, B, Expression) :-
+	bool(A), 
+	bool(B),
+	evaluation(Expression, Res),
+	write(A), 
+	write('\t'), 
+	write(B), 
+	write('\t'),
+	write(Res),
+	nl, fail.
+
+% ?- truth_table(A, B, and(xor(A, B), or(A, B))).
+% true    true    false
+% true    false   true
+% false   true    true
+% false   false   false
+% false.
