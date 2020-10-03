12fb55a3540446427f06400947ec12f5e898297c
Oleg Kuzmin <Oleg.Kuzmin@emc.com>
2020-10-03 14:51:33 +0300

    Add scala/1 solution

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,5 +1,5 @@
 package recfun
-import common._
+import scala.annotation.tailrec
 
 object Main {
   def main(args: Array[String]) {
@@ -15,14 +15,23 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if ((c == r) || (c == 0)) 1 else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    count(chars, 0) == 0;
+  }
+
+  @tailrec
+  def count(chars: List[Char], counter: Int): Int = {
+    if (chars.isEmpty || (counter < 0)) {
+      return counter
+    }
+    val popped :: newList = chars
+    count(newList, if (popped == '(') counter + 1 else if (popped == ')') counter - 1 else counter)
   }
 
   /**
@@ -33,6 +42,13 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    coins.map(coin => if (money - coin > 0) {
+      countChange(money - coin, coins.filter(elem => elem <= coin))
+    } else if (money - coin == 0) {
+      1
+    } else {
+      0
+    }).sum
   }
 }
+
