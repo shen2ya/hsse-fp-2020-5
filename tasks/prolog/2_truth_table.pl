32fa008ad9797d495e8cc8e50732d09d402efa1d
Philipp <Crisis4433@gmail.com>
2020-10-05 10:16:51 +0300

    Added lab 2 [PROLOG]

--- tasks/prolog/2_truth_table.pl
+++ tasks/prolog/2_truth_table.pl
@@ -10,3 +10,27 @@
 % true fail true
 % fail true fail
 % fail fail fail
+
+bool(true).
+bool(false).
+
+not(A) :- \+ A.
+and(A, B) :- A, B.
+or(A, B) :- not(and(not(A),	not(B))).
+xor(A, B) :- or(and(A, not(B)), and(not(A), B)).
+equ(A, B) :- not(xor(A, B)).
+
+evaluation(E, true) :- E, !.
+evaluation(E, false) :- not(E), !.
+
+truth_table(A, B, E) :-
+	bool(A), bool(B),
+	evaluation(E, Res),
+	write(A), write(' '), write(B),
+	write(Res),
+	nl, fail.
+
+% ТЕСТЫ
+% Таблица истинности: truth_table(A, B, not(xor(A, B))) true	true	true true	false	false false	true false false	false	true false.
+
+% Таблица истинности: truth_table(A, B, xor(or(A, B), and(A, B))) true	true	false true	false	true false	true	true false	false	false false.
